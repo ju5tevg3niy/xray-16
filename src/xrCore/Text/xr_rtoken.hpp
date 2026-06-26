637690267463c2de07f6d1932769193d442f320c
Evgeniy Vasilyev <evgeniy.vaslv@gmail.com>
2026-06-26 22:10:52 +0300

    chore: reformat cpp/hpp

--- src/xrCore/Text/xr_rtoken.hpp
+++ src/xrCore/Text/xr_rtoken.hpp
@@ -2,14 +2,12 @@
 #include "Common/types.hpp"
 #include "xrCore/xrstring.h"
 
-struct xr_rtoken
-{
-    shared_str name;
-    int id;
+struct xr_rtoken {
+  shared_str name;
+  int id;
 
-    xr_rtoken(pcstr _nm, int _id)
-        : name(_nm), id(_id) {}
+  xr_rtoken(pcstr _nm, int _id) : name(_nm), id(_id) {}
 
-    void rename(pcstr _nm) { name = _nm; }
-    bool equal(pcstr _nm) const { return (0 == xr_strcmp(name.c_str(), _nm)); }
+  void rename(pcstr _nm) { name = _nm; }
+  bool equal(pcstr _nm) const { return (0 == xr_strcmp(name.c_str(), _nm)); }
 };
--- src/xrCore/Utils/destructor.hpp
+++ src/xrCore/Utils/destructor.hpp
@@ -2,12 +2,11 @@
 #include "xrCore/xrMemory.h"
 
 template <class T>
-class destructor
-{
-    T* ptr;
+class destructor {
+  T* ptr;
 
-public:
-    destructor(T* p) { ptr = p; }
-    ~destructor() { xr_delete(ptr); }
-    T& operator()() { return *ptr; }
+ public:
+  destructor(T* p) { ptr = p; }
+  ~destructor() { xr_delete(ptr); }
+  T& operator()() { return *ptr; }
 };
